dc7821a905c92f53e35917a19bdb932c0e581d24
Alexander Morris <qd01a@yourika.com>
2017-07-03 15:40:13 -0400

    Create HideResteems.js

---
+++ HideResteems.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name         Hide ReSteems
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Button to Toggle ReSTEEMs from a User's STEEMIT.com Profile Page
+// @author       @alexpmorris
+// @match        https://steemit.com/*
+// @grant        none
+// @require https://code.jquery.com/jquery-1.12.4.min.js
+// @require https://gist.github.com/raw/2625891/waitForKeyElements.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var isHiding = false;
+
+    waitForKeyElements ("#posts_list", addReSteemToggleBtn);
+
+    function addReSteemToggleBtn(userDiv) {
+
+        var validUrl = document.URL.replace("https://steemit.com/","");
+
+        if ((userDiv != null) && (validUrl.startsWith("@")) && (validUrl.indexOf("/")==-1)) {
+
+            isHiding = false;
+            var zNode       = document.createElement ('div');
+            zNode.innerHTML = '<button id="rsButton" type="button"><img id="rsBtnImg" src="https://steemitimages.com/DQmQYXHkLv4A3h8pZ1ntQM1FTTT6knt5EaVUo7hdj2nNAcR/button_hide-resteems.png"></button>';
+            zNode.setAttribute ('id', 'rsContainer');
+            zNode.setAttribute ('style', 'width:120px; margin-bottom:5px;');
+            userDiv.prepend(zNode);
+
+            //activate new button
+            document.getElementById ("rsButton").addEventListener (
+                "click", ButtonClickAction, false);
+
+            function ButtonClickAction (zEvent) {
+                if (!isHiding) {
+                    $("#rsBtnImg").attr('src', 'https://steemitimages.com/DQmaRcPxCKNV45aPVaWMbBkP7WvJatgkKqtih7ZCfVsLs4r/button_show-resteems.png');
+                    $(".PostSummary__reblogged_by").parent('').hide();
+                } else {
+                    $("#rsBtnImg").attr('src', 'https://steemitimages.com/DQmQYXHkLv4A3h8pZ1ntQM1FTTT6knt5EaVUo7hdj2nNAcR/button_hide-resteems.png');
+                    $(".PostSummary__reblogged_by").parent('').show();
+                }
+                isHiding = !isHiding;
+
+            }
+
+        }
+    }
+
+})();
