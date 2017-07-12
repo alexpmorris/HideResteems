9ee186f4958fe881372d52012cc30ad4b6a81f19
Alexander Morris <qd01a@yourika.com>
2017-07-12 13:14:01 -0400

    v0.12
    added support for golos.io, and button language support as well

--- HideResteems.js
+++ HideResteems.js
@@ -2,9 +2,10 @@
 // @name         Hide ReSteems
 // @namespace    http://tampermonkey.net/
 // @version      0.12
-// @description  Button to Toggle ReSTEEMs from a User's STEEMIT.com Profile and Feed Page
+// @description  Button to Toggle ReSTEEMs from a User's steemit.com or golos.io Profile and Feed Pages
 // @author       @alexpmorris
 // @match        https://steemit.com/*
+// @match        https://golos.io/*
 // @grant        none
 // @require https://code.jquery.com/jquery-1.12.4.min.js
 // @require https://greasyfork.org/scripts/6250-waitforkeyelements/code/waitForKeyElements.js?version=23756
@@ -19,13 +20,25 @@
 
     function addReSteemToggleBtn(userDiv) {
 
-        var validUrl = document.URL.replace("https://steemit.com/","");
+        var validUrl = document.URL;
+        if (validUrl.startsWith("https://s")) validUrl = validUrl.replace("https://steemit.com/",""); else
+            validUrl = validUrl.replace("https://golos.io/","");
+
+        //language support for buttons
+        var lang = localStorage.getItem("language");
+        if ((lang == null) || (localStorage.getItem("language") == "en")) {
+            var showResteemsBtnSrc = "https://steemitimages.com/DQmaRcPxCKNV45aPVaWMbBkP7WvJatgkKqtih7ZCfVsLs4r/button_show-resteems.png";
+            var hideResteemsBtnSrc = "https://steemitimages.com/DQmQYXHkLv4A3h8pZ1ntQM1FTTT6knt5EaVUo7hdj2nNAcR/button_hide-resteems.png";
+        } else {
+            var showResteemsBtnSrc = "https://steemitimages.com/DQmQeP7KVWpKvm3eNepS8HuwHri6BAJaa8XLYvsyXUWP8E6/button_show-resteems_ru.png";
+            var hideResteemsBtnSrc = "https://steemitimages.com/DQmVds1u5g4W1jRnLNtYDEK8Bnt8WaiA3ueg81HMY3fssav/button_hide-resteems_ru.png";
+        }
 
         if ((userDiv !== null) && (validUrl.startsWith("@")) && ((validUrl.indexOf("/")==-1) || (validUrl.endsWith("/feed"))) ) {
 
             isHiding = false;
             var zNode       = document.createElement ('div');
-            zNode.innerHTML = '<button id="rsButton" type="button"><img id="rsBtnImg" src="https://steemitimages.com/DQmQYXHkLv4A3h8pZ1ntQM1FTTT6knt5EaVUo7hdj2nNAcR/button_hide-resteems.png"></button>';
+            zNode.innerHTML = '<button id="rsButton" type="button"><img id="rsBtnImg" src="'+hideResteemsBtnSrc+'"></button>';
             zNode.setAttribute ('id', 'rsContainer');
             zNode.setAttribute ('style', 'width:120px; margin-bottom:5px;');
             userDiv.prepend(zNode);
@@ -36,11 +49,11 @@
 
             function ButtonClickAction (zEvent) {
                 if (!isHiding) {
-                    $("#rsBtnImg").attr('src', 'https://steemitimages.com/DQmaRcPxCKNV45aPVaWMbBkP7WvJatgkKqtih7ZCfVsLs4r/button_show-resteems.png');
+                    $("#rsBtnImg").attr('src', showResteemsBtnSrc);
                     if (validUrl.endsWith("/feed")) $(".PostSummary__reblogged_by").parent('').hide(); else
                         $(".PostSummary__reblogged_by").filter(function () {return ($(".UserNames",this)[0] == null);}).parent('').hide();
                 } else {
-                    $("#rsBtnImg").attr('src', 'https://steemitimages.com/DQmQYXHkLv4A3h8pZ1ntQM1FTTT6knt5EaVUo7hdj2nNAcR/button_hide-resteems.png');
+                    $("#rsBtnImg").attr('src', hideResteemsBtnSrc);
                     $(".PostSummary__reblogged_by").parent('').show();
                 }
                 isHiding = !isHiding;
